eaf7ad25c3b6388a569f37e93aff0588a4984aed
Андрей <ProGlot003@yandex.ru>
2019-01-15 20:27:47 +0300

    0.0032

---
+++ menu.py
@@ -0,0 +1,58 @@
+import pygame
+import sys
+
+pygame.font.init()
+
+color = (51, 51, 51)
+font_color = (255, 255, 153)
+high_color = (153, 102, 255)
+font = pygame.font.SysFont('arial', 72)
+surface_width = 500
+surface_height = 500
+
+surface_menu = pygame.display.set_mode([surface_width, surface_height])
+
+pygame.display.set_caption("Test")
+
+surface_menu.fill(color)
+
+
+class Text():
+    def __init__(self, text, font, surface_menu, x, y):
+        self.name = text
+        self.text_obj = font.render(text, 1, font_color)
+        self.text_rect = self.text_obj.get_rect()
+        self.text_rect.topleft = (x, y)
+        surface_menu.blit(self.text_obj, self.text_rect)
+
+    def get_click(self, mouse_position):
+        x, y = mouse_position
+        if self.text_rect.topleft[0] < x <= self.text_rect.topleft[0] + self.text_rect.w and self.text_rect.topleft[
+            1] < y <= self.text_rect.topleft[1] + self.text_rect.h:
+            print("Click in", self.name)
+            print(y, self.text_rect.topleft[1], self.text_rect.h,
+                  surface_height)  # Смотреть на разницу на экране(строки10,11,41,42,30)
+            print(x, self.text_rect.topleft[0], self.text_rect.w, surface_width)
+            if (self.text_rect.topleft[0] - self.text_rect.topleft[
+                1]) % 5 == 0:  # Тут расчет как активировать одну из кнопак и действие при их нажатии
+                print('Все ок!')
+            elif (self.text_rect.topleft[0] - self.text_rect.topleft[1]) % 5 == 0:
+                print('exit')
+
+
+position_text = [((surface_width / 2) - 65, (surface_height / 2) - 90),
+                 ((surface_width / 2) - 50, (surface_height / 2) + 10)]
+start = Text('Start', font, surface_menu, *position_text[0])
+exit = Text('Exit', font, surface_menu, *position_text[1])
+
+pygame.display.update()
+running = True
+while running:
+    for event in pygame.event.get():
+        if pygame.event.wait().type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            start.get_click(event.pos)
+            exit.get_click(event.pos)
+    pygame.display.flip()
+pygame.quit()
---
+++ test.py
@@ -0,0 +1,57 @@
+import pygame
+import sys
+import os
+FPS = 50
+pygame.init()
+size = width, height = (500, 500)
+screen = pygame.display.set_mode(size)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
